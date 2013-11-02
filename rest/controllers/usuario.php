0cd2759278b2174eb4f35a99700976d0d706115f
Victor <vfajardosanchez@gmail.com>
2013-11-01 23:52:55 -0500

    método login

--- rest/controllers/usuario.php
+++ rest/controllers/usuario.php
@@ -1,20 +1,36 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class hola extends padre {
+require APPPATH . '/libraries/REST_Controller.php';
+
+class usuario extends REST_Controller {
 
 	public function index()
 	{
-
             //$this->loadHTML("booking");
             //$this->load->view('booking');
 	}
-	public function mundo($nombre,$apellido)
+	public function login_post()
 	{
-		$this->loadHTML('holamundo',array(
-		"nombre"=> $nombre, "apellido"=> $apellido),"shared/header","shared/footer");
+		$mivar=$this->post();		
+		
+		$this->db->select('aplicacionregistrada.*');
+		$this->db->from('aplicacionregistrada');
+		$this->db->join('usuario','usuario.IdPersona = aplicacionregistrada.Usuario_IdPersona');
+		$this->db->join('aplicacion','aplicacion.IdAplicacion = aplicacionregistrada.Aplicacion_IdAplicacion');
+		$this->db->where('usuario.NombreUsuario',$mivar[0]["username"]);
+		$this->db->where('usuario.Contrasena',$mivar[0]["password"]);
+		$this->db->where('aplicacion.IdAplicacion',$mivar[0]["idapp"]);
+		$query=$this->db->get()->result();
+		//print_r($query);
 
-        //$this->loadHTML("booking");
-        //$this->load->view('booking');
+		if (count($query) == 0) {
+			//print_r('No se encontraron resultados.');
+			$this->response(null,200);
+		}
+		else
+		{
+			$this->response($query,200);
+		}
 	}
 }
 
